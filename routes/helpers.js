57c449549d7bd9f80eaa044d63fc64edfb44928f
alanrmendez <alan.mendez07@gmail.com>
2022-03-29 23:27:46 -0400

    Refactor code

--- routes/helpers.js
+++ routes/helpers.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const helpers = express();
 
-
-//Determine the total number of rewards points the user has available to spend
 function sumPoints(transactions) {
   let sum = 0;
   for (let i = 0; i < transactions.length; i++) {
@@ -11,82 +9,94 @@ function sumPoints(transactions) {
   return sum;
 }
 
-
-//spendPoints function --> spend points using the given rules (oldest points spent first, no payer's points can go negative)
 function spendPoints(points, transactions) {
-
   let pointsToSpend = points;
   let availablePoints = sumPoints(transactions);
-
   if (pointsToSpend < 0) {
-    console.log('Eror: no spending negative points!');
-    return;
+    const error = 'Eror: no spending negative points!';
+    return error;
   } else if (pointsToSpend == 0) {
-    console.log('No points spent! Was this on purpose?');
-    return;
+    const response = 'No points spent! Was this on purpose?';
+    return response;
   } else if (pointsToSpend > availablePoints) {
-    console.log('Sorry, the number of points entered is greater than the number of points available to spend.');
-    return;
-  }
-  else {
-
-    // Create a list of payers as we go through each transaction
+    const error = 'Sorry, the number of points entered is greater than the number of points available to spend.';
+    return error;
+  } else {
     const payers = [];
     const payerListWithPointsSpent = [];
-
-    const sortedTransactions = transactions.sort((a, b) => a.timestamp - b.timestamp);
-    // Note: adding slice() before sort() would create a clone of the transactions array,
-    // which will be useful in the future if we want to have a standing ledger.
-
+    transactions.sort((a, b) => {
+      const firstItem = a.timestamp;
+      const secondItem = b.timestamp;
+      if (firstItem < secondItem) {
+        return -1;
+      }
+      if (firstItem > secondItem) {
+        return 1;
+      }
+      return 0;
+    });    
+    let newPayer = 0;
     while (pointsToSpend > 0) {
-
-      // If the number of points to spend is greater than the points in the oldest transaction:
-      if (pointsToSpend >= sortedTransactions[0].points) {
-        pointsToSpend -= sortedTransactions[0].points;
-
-        // Add payer to payer array and list if they aren't already in it
-        const isInList = payers.includes(sortedTransactions[0].payer);
+      for (let i = 0; i < transactions.length; i++) {
+        newPayer = transactions[i].payer;
+        const isInList = payers.includes(newPayer);
         if (isInList == false) {
-          let newPayer = sortedTransactions[0].payer;
           payers.push(newPayer);
           let newEntry = {"payer": newPayer, "points": 0};
           payerListWithPointsSpent.push(newEntry);
         }
-
-        // Find the appropriate payer in the list and subtract the number of points spent
-        for (let i = 0; i < payerListWithPointsSpent.length; i++) {
-          if (newPayer == payerListWithPointsSpent[i].payer) {
-            payerListWithPointsSpent[i].points -= sortedTransactions[0].points;
+        if (pointsToSpend >= transactions[i].points) {
+          pointsToSpend -= transactions[i].points;
+          for (let j = 0; j < payerListWithPointsSpent.length; j++) {
+            if (payerListWithPointsSpent[j].payer == newPayer) {
+              payerListWithPointsSpent[j].points -= transactions[i].points;
+            }
           }
-        }
-
-        // Delete the oldest transaction (first in the array)
-        sortedTransactions.shift();
-
-        if (pointsToSpend == 0) {
+          transactions[i].points = 0;
+          if (pointsToSpend == 0) {
+            break;
+          }
+        } else { 
+          transactions[i].points -= pointsToSpend;
+          for (let j = 0; j < payerListWithPointsSpent.length; j++) {
+            if (payerListWithPointsSpent[j].payer == newPayer) {
+              payerListWithPointsSpent[j].points -= pointsToSpend;
+            }
+          }
+          pointsToSpend = 0;
           break;
         }
-
-      } else { 
-
-        // If the number of points to spend is less than the points in the oldest transaction
-        sortedTransactions[0].points -= pointsToSpend;
-
-        // Find the appropriate payer in the list and subtract the number of points spent
-        for (let i = 0; i < payerListWithPointsSpent.length; i++) {
-          if (newPayer == payerListWithPointsSpent[i].payer) {
-            payerListWithPointsSpent[i].points -= pointsToSpend;
-          }
-        }
-
-        pointsToSpend = 0;
       }
-      
     }
-
     return payerListWithPointsSpent;
-
   }
 }
 
-module.exports = helpers;
+function pointsBalance(transactions) {
+  const payers = [];
+  const balances = [];
+  if (transactions.length == 0) {
+    const result = 'You don\'t have any available points.';
+    return result;
+  } else {
+    for (let i = 0; i < transactions.length; i++) {
+      let newPayer = transactions[i].payer;
+      let pointsToAdd = transactions[i].points;
+      const isInList = payers.includes(newPayer);
+      if (isInList == false) {
+        payers.push(newPayer);
+        let newEntry = {"payer": newPayer, "points": 0};
+        balances.push(newEntry);
+      }
+      for (let j = 0; j < balances.length; j++) {
+        if (balances[j].payer == newPayer) {
+          balances[j].points += pointsToAdd;
+        }
+      }
+    }
+  }
+  return balances;
+}
+
+module.exports.spendPoints = spendPoints;
+module.exports.pointsBalance = pointsBalance;
